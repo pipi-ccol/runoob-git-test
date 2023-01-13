d3ccb432b3d6059917723bbdeb03c97d67022514
dsqzqm <2564305991@qq.com>
2023-01-13 13:00:58 +0800

    main页面完成，main跳转接口给出【home.java】

--- app/src/main/java/com/djht/fourteenth_session/Fragment/home_D.java
+++ app/src/main/java/com/djht/fourteenth_session/Fragment/home_D.java
@@ -11,10 +11,30 @@ import androidx.fragment.app.Fragment;
 
 import com.djht.fourteenth_session.R;
 
-public class home_D extends Fragment {
+public class home_D extends Fragment implements View.OnClickListener{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_d, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            //灯控制页面跳转
+            case R.id.main_light:
+                //跳转逻辑
+                break;
+            case R.id.main_electronic_door_lock:
+                break;
+            case R.id.main_air_conditioning:
+                break;
+            case R.id.main_smart_optimization:
+                break;
+            case R.id.main_more_three:
+                break;
+            default:
+                break;
+        }
     }
 }
