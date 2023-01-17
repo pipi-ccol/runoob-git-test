e15fb228c19a0782e3a9b0eda634919a1a432346
dsqzqm <2564305991@qq.com>
2023-01-17 13:20:58 +0800

    light框架制作

---
+++ app/src/main/java/com/djht/fourteenth_session/DSQ/light.java
@@ -0,0 +1,91 @@
+package com.djht.fourteenth_session.DSQ;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.djht.fourteenth_session.R;
+import com.djht.fourteenth_session.Utils.Image_chage_D;
+
+import java.io.InputStream;
+
+public class light extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView light_back;
+    private ImageView light_add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light);
+        initView();
+    }
+
+    /**
+     * 制作顶部导航栏
+     * @param hasFocus
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().setStatusBarColor(Color.parseColor("#5DE6D6"));
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            actionBar.setCustomView(R.layout.light_action_bar);
+            Resources resources = light.this.getResources();
+            Drawable drawable = resources.getDrawable(R.drawable.light_action_bar);
+            actionBar.setBackgroundDrawable(drawable);
+
+            ImageView light_back= (ImageView) findViewById(R.id.light_back);
+            light_back.setOnClickListener(this);
+
+            ImageView light_add= (ImageView) findViewById(R.id.light_add);
+            light_add.setOnClickListener(this);
+
+            Resources r = this.getResources();
+            @SuppressLint("ResourceType") InputStream is = r.openRawResource(R.mipmap.light_back);
+            BitmapDrawable bmpview = new BitmapDrawable(is);
+            Bitmap bmp = Image_chage_D.createCircleImage(Image_chage_D.zoomImg(bmpview.getBitmap(),50,50));
+            light_back.setImageBitmap(bmp);
+            Resources r1 = this.getResources();
+
+            @SuppressLint("ResourceType") InputStream is1 = r1.openRawResource(R.mipmap.light_add);
+            BitmapDrawable bmpview1 = new BitmapDrawable(is1);
+            Bitmap bmp1 = Image_chage_D.createCircleImage(Image_chage_D.zoomImg(bmpview1.getBitmap(),50,50));
+            light_add.setImageBitmap(bmp1);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.light_back:
+                this.finish();
+                break;
+            case R.id.light_add:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initView() {
+
+    }
+}
--- app/src/main/java/com/djht/fourteenth_session/Fragment/home_D.java
+++ app/src/main/java/com/djht/fourteenth_session/Fragment/home_D.java
@@ -6,27 +6,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 
+import com.djht.fourteenth_session.DSQ.light;
 import com.djht.fourteenth_session.HJK.air_condition_main_h;
 import com.djht.fourteenth_session.R;
 
 public class home_D extends Fragment implements View.OnClickListener{
+
+    private ImageButton main_light;
+    private ImageButton main_air_conditioning;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_d, container, false);
-        View main_air_conditioning = view.findViewById(R.id.main_air_conditioning);
-        main_air_conditioning.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        initView();
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private void initView(){
+        main_air_conditioning = getActivity().findViewById(R.id.main_air_conditioning);
+        main_air_conditioning.setOnClickListener(this);
+        main_light=getActivity().findViewById(R.id.main_light);
+        main_light.setOnClickListener(this);
     }
 
     @Override
@@ -35,6 +49,8 @@ public class home_D extends Fragment implements View.OnClickListener{
             //灯控制页面跳转
             case R.id.main_light:
                 //跳转逻辑
+                Intent light = new Intent(this.getActivity(), light.class);
+                startActivity(light);
                 break;
             case R.id.main_electronic_door_lock:
                 break;
